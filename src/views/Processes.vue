4a60333bef06bd858e0e9152b434725317e25458
yonejima <catchandrelease9@gmail.com>
2020-01-25 19:47:44 +0900

    Processes.vueを追加

---
+++ src/views/Processes.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="processes">
+    <HomeTable />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HomeTable from "@/components/HomeTable.vue";
+
+export default {
+  name: "processes",
+  components: {
+    HomeTable
+  }
+};
+</script>
+
