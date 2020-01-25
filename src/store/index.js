4a8a9c06498797de6f52adc19702b50318ff05fc
yonejima <catchandrelease9@gmail.com>
2020-01-25 22:13:15 +0900

    storeの記述を追加

---
+++ src/store/index.js
@@ -0,0 +1,57 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import router from '../router';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    idToken: null,
+  },
+  getters: {
+    idToken: state => state.idToken,
+  },
+  mutations: {
+    updateIdToken(state, idToken) {
+      state.idToken = idToken;
+    }
+  },
+  actions: {
+    autoLogin({ commit, dispatch }) {
+      const idToken = localStorage.getItem('idToken');
+      if (!idToken) return;
+      const now = new Date();
+      const expiryTimeMs = localStorage.getItem('expiryTimeMs');
+      const isExpired = now.getTime() >= expiryTimeMs;
+      const refreshToken = localStorage.getItem('refreshToken');
+      if (isExpired) {
+        dispatch('refreshIdToken', refreshToken)
+      } else {
+        const expiresInMs = expiryTimeMs - now.getTime();
+        setTimeout(() => {
+          dispatch('refreshIdToken', refreshToken)
+        }, expiresInMs)
+        commit('updateIdToken', idToken)
+      }
+    },
+    logout({ commit }) {
+      commit('updateIdToken', null);
+      localStorage.removeItem('idToken');
+      localStorage.removeItem('expiryTimeMs');
+      localStorage.removeItem('refreshToken');
+      router.replace('/login');
+    },
+    setAuthData({ commit, dispatch }, authData) {
+      const now = new Date();
+      const expiryTimeMs = now.getTime() + authData.expiresIn * 1000
+      commit('updateIdToken', authData.idToken);
+      localStorage.setItem('idToken', authData.idToken);
+      localStorage.setItem('expiryTimeMs', expiryTimeMs);
+      localStorage.setItem('refreshToken', authData.refreshToken);
+      setTimeout(() => {
+        dispatch('refreshIdToken', authData.refreshToken)
+      }, authData.expiresIn * 1000);
+
+    }
+  }
+})
