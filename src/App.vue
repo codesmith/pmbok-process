ed929bd320cb22808f221e047420cb1d6bc5e7cf
yonejima <catchandrelease9@gmail.com>
2020-02-08 22:27:30 +0900

    navにツールチップを追加

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,11 @@
     <div id="nav">
       <router-link to="/home">Home</router-link>|
       <router-link to="/processes">Processes</router-link>|
-      <router-link to="/itto">ITTO</router-link>
+      <router-link to="/itto">ITTO</router-link>|
+      <div class="tooltip1">
+        <p>色分け</p>
+        <div class="description1">説明を入力してください。</div>
+      </div>
     </div>
     <transition mode="out-in">
       <router-view />
@@ -11,7 +15,7 @@
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -33,6 +37,12 @@
   color: #2c3e50;
 }
 
+#nav p {
+  font-weight: bold;
+  color: #2c3e50;
+  padding: 5px;
+}
+
 #nav a.router-link-exact-active {
   color: #42b983;
 }
@@ -44,5 +54,41 @@
 .v-enter,
 .v-leave-to {
   opacity: 0;
+}
+.tooltip1 {
+  position: relative;
+  cursor: pointer;
+  display: inline-block;
+}
+.tooltip1 p {
+  margin: 0;
+  padding: 0;
+  text-decoration: underline;
+}
+.description1 {
+  display: none;
+  position: absolute;
+  padding: 10px;
+  font-size: 12px;
+  line-height: 1.6em;
+  color: #fff;
+  border-radius: 5px;
+  background: #000;
+  width: 100px;
+}
+.description1:before {
+  content: "";
+  position: absolute;
+  top: 0%;
+  right: 95%;
+  border: 15px solid transparent;
+  border-top: 15px solid #000;
+  margin-left: -15px;
+  transform: rotateZ(90deg);
+}
+.tooltip1:hover .description1 {
+  display: inline-block;
+  top: 0px;
+  left: 80px;
 }
 </style>
